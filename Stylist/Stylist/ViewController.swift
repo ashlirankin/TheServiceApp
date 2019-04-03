e709790d1a9ac036d657c65bbbfa7eafab761e84
Ashlirankin18 <ashlirankin18@gmail.com>
2019-04-03 14:24:47 -0400

    pushing the xcodefile

---
+++ Stylist/Stylist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stylist
+//
+//  Created by Ashli Rankin on 4/3/19.
+//  Copyright © 2019 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
