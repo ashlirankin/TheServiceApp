63282195c91f148ec4f57edcb29fd24eca045918
Ashlirankin18 <ashlirankin18@gmail.com>
2019-04-03 22:37:49 -0400

    created the model files and created the mvc folder layouts.

---
+++ Stylist/Stylist/Controllers/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Stylist
+//
+//  Created by Ashli Rankin on 4/3/19.
+//  Copyright © 2019 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Stylist/Stylist/Models/StylistsUsers.swift
@@ -0,0 +1,9 @@
+//
+//  StylistsUsers.swift
+//  Stylist
+//
+//  Created by Ashli Rankin on 4/3/19.
+//  Copyright © 2019 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
