dbfa6346362c39094644a11137d4c827071901d7
Ashlirankin18 <ashlirankin18@gmail.com>
2019-04-03 23:06:15 -0400

    creted a model for service provider avalibility

---
+++ Stylist/Stylist/Models/Avalibility.swift
@@ -0,0 +1,12 @@
+//
+//  Avalibility.swift
+//  Stylist
+//
+//  Created by Ashli Rankin on 4/3/19.
+//  Copyright © 2019 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
+struct Avalibility {
+  
+}
