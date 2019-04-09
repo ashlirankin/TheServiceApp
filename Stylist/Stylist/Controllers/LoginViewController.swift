2bd631cfdf228318baad3c7a5793a4df72b62e95
Ashlirankin18 <ashlirankin18@gmail.com>
2019-04-09 16:32:40 -0400

    created many files

--- Stylist/Stylist/Controllers/LoginViewController.swift
+++ Stylist/Stylist/Controllers/LoginViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: BaseViewController {
 
   @IBOutlet weak var emailTextField: UITextField!
   @IBOutlet weak var passwordTextfield: UITextField!
   
-  
   let authService = AuthService()
-  var accountState:AccountState = .serviceProvider
   override func viewDidLoad() {
         super.viewDidLoad()
 
       authService.authserviceExistingAccountDelegate = self
+    createGradientView()
     }
-    
+  
+
   @IBAction func loginButtonPressed(_ sender: UIButton) {
     guard let email = emailTextField.text,
       let password = passwordTextfield.text else {
@@ -35,28 +35,14 @@ class LoginViewController: UIViewController {
   }
   
   @IBAction func segmentedControlPressed(_ sender: UISegmentedControl) {
-    switch sender.selectedSegmentIndex {
-     
-    case 0:
-      accountState = .serviceProvider
-    case 1:
-      accountState = .consumer
-    default:
-      print("c")
-    }
+    
   }
   
   func presentTabbarController(){
-    if accountState == .consumer {
+   
       guard let userTabbarController = UIStoryboard.init(name: "User", bundle: nil).instantiateViewController(withIdentifier: "UserTabBarController") as? UITabBarController else {return}
       self.present(userTabbarController, animated: true, completion: nil)
-      
-    }else if accountState == .serviceProvider{
-      
-      guard let serviceTabbarController = UIStoryboard.init(name: "ServiceProvider", bundle: nil).instantiateViewController(withIdentifier: "ServiceTabBar") as? UITabBarController else {return}
-      present(serviceTabbarController, animated: true, completion: nil)
-      
-    }
+    
   }
   
  
