406afe4e42332860e225a94dbbd5a5d5bff8ff37
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-04-10 12:12:32 -0400

    User is able to change profile pic through camera / photolibrary

--- Stylist/Stylist/Controllers/ClientEditProfileController.swift
+++ Stylist/Stylist/Controllers/ClientEditProfileController.swift
@@ -11,11 +11,12 @@ import Toucan
 
 class ClientEditProfileController: UITableViewController {
     
-    
     @IBOutlet weak var profileImageView: CircularImageView!
     @IBOutlet weak var nameTextFields: UITextField!
     
-    private lazy var imagePickerView: UIImagePickerController = {
+    public var stylistUser: StylistsUser!
+    
+    private lazy var imagePickerController: UIImagePickerController = {
         let ip = UIImagePickerController()
         ip.delegate = self
         return ip
@@ -25,33 +26,34 @@ class ClientEditProfileController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        if let imageURL = stylistUser.imageURL {
+            profileImageView.kf.indicatorType = .activity
+            profileImageView.kf.setImage(with: URL(string: imageURL), placeholder: #imageLiteral(resourceName: "placeholder.png"))
+        }
+        nameTextFields.text = stylistUser.fullName
     }
     
     @IBAction func changeProfilePicButtonPressed(_ sender: UIButton) {
-//        var actionTitles = [String]()
-//        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-//            actionTitles = ["Photo Library", "Camera"]
-//        } else {
-//            actionTitles = ["Photo Library"]
-//        }
-//        showActionSheet(title: nil, message: nil, actionTitles: actionTitles, handlers: [{ [unowned self] photoLibraryAction in
-//            self.imagePickerController.sourceType = .photoLibrary
-//            self.present(self.imagePickerController, animated: true)
-//
-//            }, { cameraAction in
-//                self.imagePickerController.sourceType = .camera
-//                self.present(self.imagePickerController, animated: true)
-//            }
-//            ])
-        // set titles
-        // check if camera is available
-        // check action sheet
+        let actionTitles = UIImagePickerController.isSourceTypeAvailable(.camera) ? ["Photo Library", "Camera"] : ["Photo Library"]
         
+        showActionSheet(title: nil, message: nil, actionTitles: actionTitles, handlers: [{ [unowned self] photoLibraryAction in
+            self.imagePickerController.sourceType = .photoLibrary
+            self.present(self.imagePickerController, animated: true)
+            }, { cameraAction  in
+                self.imagePickerController.sourceType = .camera
+                self.present(self.imagePickerController, animated: true)
+            }
+            ])
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+        // Lots of Code HERE!!!
+        // Update firebase
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -60,27 +62,19 @@ class ClientEditProfileController: UITableViewController {
 }
 
 extension ClientEditProfileController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//        guard let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-//            print("original image not available")
-//            return
-//        }
-//        let size = CGSize(width: 500, height: 500)
-//        let resizedImage = Toucan.Resize.resizeImage(originalImage, size: size)
-//        switch imageEditingState {
-//        case .profileImageEditing?:
-//            selectedProfileImage = resizedImage
-//            profileImageButton.setImage(resizedImage, for: .normal)
-//        case .coverImageEditing?:
-//            selectedCoverImage = resizedImage
-//            profileCoverImageButton.setImage(resizedImage, for: .normal)
-//        default:
-//            break
-//        }
-//        dismiss(animated: true)
+        guard let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            showAlert(title: "Image Not Available", message: nil, actionTitle: "OK")
+            return
+        }
+        let size = CGSize(width: 500, height: 500)
+        let resizedImage = Toucan.Resize.resizeImage(originalImage, size: size)
+        selectedImage = resizedImage
+        dismiss(animated: true)
     }
 }
