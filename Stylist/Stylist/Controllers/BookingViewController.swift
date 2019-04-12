85a0870ff6dbf823b3f9127520668e4209386153
Ashlirankin18 <ashlirankin18@gmail.com>
2019-04-12 14:44:22 -0400

    created the book feature storyboard and view controller

---
+++ Stylist/Stylist/Controllers/BookingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BookingViewController.swift
+//  Stylist
+//
+//  Created by Ashli Rankin on 4/12/19.
+//  Copyright © 2019 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class BookingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+}
