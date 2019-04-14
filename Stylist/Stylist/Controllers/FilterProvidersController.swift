08fe154faab56ca4fccfd6599b57f7563f1ba101
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-04-14 18:40:37 -0400

    Added Files for Filter Providers Storyboard

--- Stylist/Stylist/Controllers/FilterProvidersController.swift
+++ Stylist/Stylist/Controllers/FilterProvidersController.swift
@@ -9,25 +9,15 @@
 import UIKit
 
 class FilterProvidersController: UITableViewController {
+    
+    
+    @IBOutlet weak var genderCell: UITableViewCell!
+    @IBOutlet weak var professionCell: UITableViewCell!
+    @IBOutlet weak var servicesCell: UITableViewCell!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        if section == 2 {
-//            return 10 // services
-//        } else {
-//            return 1
-//        }
-//    }
-    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.section == 2 {
-////            let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: <#T##IndexPath#>)
-//            return UITableViewCell()
-//        }
-//        return UITableViewCell()
-//    }
 
 }
