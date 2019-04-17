3a82a1d3613eb4634fd6d4b236e5ef40f4f4c774
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-04-17 17:26:24 -0400

    reset UserDefaults Added

--- Stylist/Stylist/SupportingFiles/UserDefaultsKeys.swift
+++ Stylist/Stylist/SupportingFiles/UserDefaultsKeys.swift
@@ -9,10 +9,20 @@
 import Foundation
 
 struct UserDefaultsKeys {
+    static let defaults = UserDefaults.standard
+    
     static let availableNow = "AvailableNow"
     static let genderFilter = "GenderFilter"
     static let professionFilter = "ProfessionFilter"
     static let priceRangeFilter = "PriceRangeFilter"
     static let servicesFilter = "ServicesFilter"
+    
+    static func wipeUserDefaults() {
+        defaults.set(false, forKey: availableNow)
+        defaults.set([String: String](), forKey: genderFilter)
+        defaults.set([String: String](), forKey: professionFilter)
+        defaults.set([String: String](), forKey: priceRangeFilter)
+        defaults.set([String: String](), forKey: servicesFilter)
+    }
 }
 
