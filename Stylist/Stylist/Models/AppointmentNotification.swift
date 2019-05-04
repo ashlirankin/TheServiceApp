3484118f54672e4854fc16ee364e79b89e1f9d3e
onielrosario <onielrosario@pursuit.org>
2019-05-04 13:49:52 -0400

    fixed notification when launch the app

--- Stylist/Stylist/Models/AppointmentNotification.swift
+++ Stylist/Stylist/Models/AppointmentNotification.swift
@@ -23,6 +23,9 @@ class AppointmentNotification {
             if let error = error {
                 print(error)
             } else if let appointments = appointments {
+                guard appointments.count > 0 else  {
+                    return 
+                }
                 self.appointments = appointments
             }
         }
