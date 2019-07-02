0c91c28a2710bf363ffce27a06a6316aaeae4d4c
onielrosario <onielrosario@pursuit.org>
2019-07-02 12:27:24 -0600

    if user already filled a form a custom view will show as support screen the provider license

---
+++ Stylist/Stylist/Views/nibs/SentForm.swift
@@ -0,0 +1,30 @@
+//
+//  SentForm.swift
+//  Stylist
+//
+//  Created by Oniel Rosario on 7/2/19.
+//  Copyright © 2019 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class SentForm: UIView {
+    @IBOutlet var content: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+   private func commonInit() {
+    Bundle.main.loadNibNamed("FormSent", owner: self, options: nil)
+    addSubview(content)
+    }
+    
+    
+}
