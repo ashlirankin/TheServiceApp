aab227c892e44593930e68146949b1e138003d0a
onielrosario <onielrosario@pursuit.org>
2019-07-06 14:29:48 -0600

    fixing new bug on vc stacks

--- Stylist/Stylist/Controllers/ServiceProfileViewController.swift
+++ Stylist/Stylist/Controllers/ServiceProfileViewController.swift
@@ -12,20 +12,26 @@ import Firebase
 class ServiceProfileViewController: UIViewController {
     @IBOutlet weak var providerImage: CircularImageView!
     @IBOutlet weak var providerName: UILabel!
-  var stylistUser: StylistsUser?{
-    didSet{
-      updateUI()
+    var stylistUser: StylistsUser?{
+        didSet{
+            updateUI()
+        }
     }
-  }
-  
+    
     var isSwitched = true
     let authservice = AuthService()
     override func viewDidLoad() {
         super.viewDidLoad()
-      getUser()
-
+        getUser()
     }
     
+    
+    override func viewDidDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+            guard let viewControllers = navigationController?.viewControllers,
+                let index = viewControllers.firstIndex(of: self) else { return }
+            navigationController?.viewControllers.remove(at: index)
+    }
     
     private func  updateUI() {
         guard let currentUser = stylistUser else  { return }
@@ -38,23 +44,23 @@ class ServiceProfileViewController: UIViewController {
             }
         }
     }
-  
-  func getUser(){
-    guard let currentUser = authservice.getCurrentUser() else {return}
-    DBService.getDatabaseUser(userID: currentUser.uid) { [weak self] (error, user) in
-      if let error = error {
-        self?.showAlert(title: "Error", message: "There was an error getting the user:\(error.localizedDescription) ", actionTitle: "Try Aagain")
-        
-      } else if let user = user {
-        self?.stylistUser = user
-      }
+    
+    func getUser(){
+        guard let currentUser = authservice.getCurrentUser() else {return}
+        DBService.getDatabaseUser(userID: currentUser.uid) { [weak self] (error, user) in
+            if let error = error {
+                self?.showAlert(title: "Error", message: "There was an error getting the user:\(error.localizedDescription) ", actionTitle: "Try Aagain")
+                
+            } else if let user = user {
+                self?.stylistUser = user
+            }
+        }
     }
-  }
     
     @IBAction func switchprofileButton(_ sender: UIButton) {
         isSwitched = !isSwitched
         if isSwitched == false {
-          showUserTab()
+            showUserTab()
         }
     }
     
@@ -63,8 +69,9 @@ class ServiceProfileViewController: UIViewController {
         let servicetab = storyboard.instantiateViewController(withIdentifier: "UserTabBarController")
         servicetab.modalTransitionStyle = .crossDissolve
         servicetab.modalPresentationStyle = .overFullScreen
-        self.present(servicetab, animated: true)
-        let appdeletgate = (UIApplication.shared.delegate) as! AppDelegate
-        appdeletgate.window?.rootViewController = servicetab
+        self.present(servicetab, animated: true) {
+            let appdeletgate = (UIApplication.shared.delegate) as! AppDelegate
+            appdeletgate.window?.rootViewController = servicetab
+        }
     }
 }
